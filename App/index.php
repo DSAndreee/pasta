a70d7cd014fa02bc03c3adb613284f4c2fa282d0
YouniS Bensalah <younis.bensalah@riseup.net>
2014-05-17 20:06:49 +0200

    No need for action=paste ?hash= is fine

--- App/index.php
+++ App/index.php
@@ -4,5 +4,5 @@ Neo\boot();
 
 Neo\id(new Neo\Neo())
 ->map('index', 'PastaC')
-->map(array('action' => 'paste', 'hash' => '[0-9a-zA-Z]{16}'), 'PastaC', 'readbox')
+->map(array('hash' => '[0-9a-zA-Z]{16}'), 'PastaC', 'readbox')
 ->run();
