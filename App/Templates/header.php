99f0cc488b54c30a7e3a9bc2fcb69a1ba2ad94d6
fabienwang <fabienwang@eliteheberg.fr>
2014-05-18 14:51:34 +0200

    Changing to CodeMirror

--- App/Templates/header.php
+++ App/Templates/header.php
@@ -5,6 +5,24 @@
     <meta name="description" content="Pasta, A pastebin for elite coders by Fabien Wang && YouniS Bensalah" />
     <meta charset="UTF-8">
     <link href="Style/neo.css" type="text/css" rel="stylesheet"/>
+
+    <link rel="stylesheet" href="Vendor/CodeMirror/lib/codemirror.css">
+    <link rel="stylesheet" href="Vendor/CodeMirror/addon/fold/foldgutter.css">
+    <link rel="stylesheet" href="Vendor/CodeMirror/addon/dialog/dialog.css">
+    <link rel="stylesheet" href="Vendor/CodeMirror/theme/monokai.css">
+    <script src="Vendor/CodeMirror/lib/codemirror.js"></script>
+    <script src="Vendor/CodeMirror/addon/search/searchcursor.js"></script>
+    <script src="Vendor/CodeMirror/addon/search/search.js"></script>
+    <!--<script src="Vendor/CodeMirror/addon/dialog/dialog.js"></script>-->
+    <script src="Vendor/CodeMirror/addon/edit/matchbrackets.js"></script>
+    <script src="Vendor/CodeMirror/addon/edit/closebrackets.js"></script>
+    <script src="Vendor/CodeMirror/addon/comment/comment.js"></script>
+    <script src="Vendor/CodeMirror/addon/wrap/hardwrap.js"></script>
+    <script src="Vendor/CodeMirror/addon/fold/foldcode.js"></script>
+    <script src="Vendor/CodeMirror/addon/fold/brace-fold.js"></script>
+    <script src="Vendor/CodeMirror/mode/php/php.js"></script>
+    <script src="Vendor/CodeMirror/keymap/sublime.js"></script>
+
 </head>
 <body>
     <div class="navleft">
