c21d0593e899fb7ecb76ffdc7808cebc46fe9e66
fabienwang <fabienwang@eliteheberg.fr>
2014-05-18 19:40:00 +0200

    Styling the editor

--- App/Templates/readbox.php
+++ App/Templates/readbox.php
@@ -3,6 +3,11 @@ if ($this->blank('paste')) {
     echo '<h1>Dat pasta iz no existingz.</h1>';
 }
 else {
-    echo '<div class="readbox">' . $this->values['paste']['content'] . '</div>';
+    //echo '<div class="readbox">' . $this->values['paste']['content'] . '</div>';
+    echo '<form method="post" name="paste">
+    <textarea id="pastecontent" name="content"></textarea>
+    <input type="submit" value="Paste">
+    </form>';
+
 }
 ?>
