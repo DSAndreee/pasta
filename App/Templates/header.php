f2190b3a9afe02f912fd946c52f2bd47452e77c0
fabienwang <fabienwang@eliteheberg.fr>
2014-05-18 04:07:31 +0200

    Began the style :P

--- App/Templates/header.php
+++ App/Templates/header.php
@@ -2,5 +2,8 @@
 <html>
 <head>
     <title>Pasta - <?php echo $this->blank('page_title') ? 'Dis post iz no exisztz' : $this->get('page_title'); ?></title>
+    <link href="Style/neo.css" type="text/css" rel="stylesheet"/>
 </head>
 <body>
+    <div class="navleft"></div>
+    <div class="editor"></div>
