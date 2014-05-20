055895eb2f347c66ccc1a75316a0e8f5ed6f4a5d
YouniS Bensalah <younis.bensalah@riseup.net>
2014-05-20 22:45:10 +0200

    Vendor because GitHub is not telling us how to name our files.

--- App/Templates/header.php
+++ App/Templates/header.php
@@ -6,28 +6,28 @@
     <meta charset="UTF-8">
     <link href="Style/neo.css" type="text/css" rel="stylesheet"/>
     <link href="Style/icons.css" type="text/css" rel="stylesheet"/>
-    <link rel="stylesheet" href="vendor/CodeMirror/lib/codemirror.css">
-    <script src="vendor/CodeMirror/lib/codemirror.js"></script>
-    <link rel="stylesheet" href="vendor/CodeMirror/theme/monokai.css">
+    <link rel="stylesheet" href="Vendor/CodeMirror/lib/codemirror.css">
+    <script src="Vendor/CodeMirror/lib/codemirror.js"></script>
+    <link rel="stylesheet" href="Vendor/CodeMirror/theme/monokai.css">
 
-    <script src="vendor/CodeMirror/addon/mode/loadmode.js"></script>
-    <script src="vendor/CodeMirror/addon/mode/overlay.js"></script>
+    <script src="Vendor/CodeMirror/addon/mode/loadmode.js"></script>
+    <script src="Vendor/CodeMirror/addon/mode/overlay.js"></script>
     <!--editor search and replace dialogs-->
-    <script src="vendor/CodeMirror/addon/search/searchcursor.js"></script>
-    <script src="vendor/CodeMirror/addon/search/search.js"></script>
+    <script src="Vendor/CodeMirror/addon/search/searchcursor.js"></script>
+    <script src="Vendor/CodeMirror/addon/search/search.js"></script>
     <!--editor options-->
-    <script src="vendor/CodeMirror/addon/edit/matchbrackets.js"></script>
-    <script src="vendor/CodeMirror/addon/edit/closebrackets.js"></script>
-    <script src="vendor/CodeMirror/addon/comment/comment.js"></script>
-    <script src="vendor/CodeMirror/addon/wrap/hardwrap.js"></script>
-    <script src="vendor/CodeMirror/addon/fold/foldcode.js"></script>
-    <script src="vendor/CodeMirror/addon/fold/brace-fold.js"></script>
+    <script src="Vendor/CodeMirror/addon/edit/matchbrackets.js"></script>
+    <script src="Vendor/CodeMirror/addon/edit/closebrackets.js"></script>
+    <script src="Vendor/CodeMirror/addon/comment/comment.js"></script>
+    <script src="Vendor/CodeMirror/addon/wrap/hardwrap.js"></script>
+    <script src="Vendor/CodeMirror/addon/fold/foldcode.js"></script>
+    <script src="Vendor/CodeMirror/addon/fold/brace-fold.js"></script>
     <!--php highlighting-->
-    <script src="vendor/CodeMirror/mode/clike/clike.js"></script>
-    <script src="vendor/CodeMirror/mode/php/php.js"></script>
-    <script src="vendor/CodeMirror/mode/xml/xml.js"></script>
-    <script src="vendor/CodeMirror/mode/htmlmixed/htmlmixed.js"></script>
+    <script src="Vendor/CodeMirror/mode/clike/clike.js"></script>
+    <script src="Vendor/CodeMirror/mode/php/php.js"></script>
+    <script src="Vendor/CodeMirror/mode/xml/xml.js"></script>
+    <script src="Vendor/CodeMirror/mode/htmlmixed/htmlmixed.js"></script>
     <!--sublime shortcuts-->
-    <script src="vendor/CodeMirror/keymap/sublime.js"></script>
+    <script src="Vendor/CodeMirror/keymap/sublime.js"></script>
 </head>
 <body>
